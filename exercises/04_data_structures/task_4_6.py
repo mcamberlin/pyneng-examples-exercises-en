c750d3e761acd9c93aed53bfb51a22efcd900dfa
Merlin <merlin.camberlin@gmail.com>
2024-08-21 19:11:39 +0200

    Review tasks 4.x

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -6,7 +6,7 @@ Process the ospf_route string and print the information to the stdout as follows
 Prefix                10.0.24.0/24
 AD/Metric             110/41
 Next-Hop              10.0.13.3
-Last update           3d18h
+Last update           3d20h
 Outbound Interface    FastEthernet0/0
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
@@ -17,10 +17,13 @@ This does not mean that the task was done correctly, it is just that at
 this stage it is difficult otherwise test the result.
 """
 
-ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d20h, FastEthernet0/0"
 tab = ospf_route.strip().replace(',','').split()
-print(f"Prefix\t{tab[0]}")
-print(f"AD/Metric\t{tab[1]}")
-print(f"Next-Hop\t{tab[3]}")
-print(f"Last Update\t{tab[4]}")
-print(f"Outbound Interface\t{tab[5]}")
+
+s = "{:<20} {}\n{:<20} {}\n{:<20} {}\n{:<20} {}\n{:<20} {}".format(
+    "Prefix", tab[0],
+    "AD/Metric", tab[1].replace('[','').replace(']',''),
+    "Next-Hop", tab[3],
+    "Last Update", tab[4],
+    "Outbound Interface", tab[5])
+print(s)
